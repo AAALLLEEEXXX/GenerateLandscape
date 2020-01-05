4f6f3f8b8255cee770bba88fa3f332a6ad87eeda
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2020-01-05 22:29:43 +0300

    terrain generation using perlin noise

---
+++ Assets/Scripts/LandScapeGenerateInspector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LandScapeGenerate))]
+public class LandScapeGenerateInspector : Editor
+{
+    LandScapeGenerate myTarget;
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        base.OnInspectorGUI();
+        myTarget = (LandScapeGenerate)target;
+        if (GUILayout.Button("Generate Landscape"))
+        {
+            myTarget.GenerateTerrain();
+        }
+        if (EditorGUI.EndChangeCheck())
+        {
+            GenerateTerrain();
+        }
+    }
+
+    private void OnEnable()
+    {
+        myTarget = (LandScapeGenerate)target;
+        Undo.undoRedoPerformed += GenerateTerrain;
+    }
+
+    private void OnDisable()
+    {
+        Undo.undoRedoPerformed -= GenerateTerrain;
+    }
+
+    private void GenerateTerrain()
+    {
+        myTarget.GenerateTerrain();
+    }
+}
